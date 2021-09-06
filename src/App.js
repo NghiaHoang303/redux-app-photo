88696f60a72c8b5adabec60034a15e78a01aed5f
nghiahoangdayoneteams <nghiahoang@dayoneteams.com>
2021-09-06 07:34:47 +0700

    config server surge

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import { Suspense } from 'react';
 import { BrowserRouter,Redirect,Switch,Route, Link } from 'react-router-dom';
 import NotFound from './components/NotFound';
 import Header from './components/header';
+import productApi from 'api/productApi';
+import SignIn from 'features/Auth/pages/SignIn';
 
 const Photo = React.lazy(() => import('./features/Photo'))
 
 function App() {
+
+  const [productList, setProductList] = useState([]);
+  useEffect(() => {
+    const fetchProductList = async () => {
+      try {
+        const params = {
+          _page : 1, 
+          _limit: 10
+        }
+        const response = await productApi.getAll(params);
+        console.log(response);
+      }catch (err) {
+        console.error('Failed to fetch product list',err);
+      }
+    }
+
+    fetchProductList();
+  }, [])
+
   return (
     <div className="photo-app">
       <Suspense fallback={<div>Loading ... </div>}>
@@ -24,6 +45,7 @@ function App() {
           <Switch>
             <Redirect exact from="/" to ="photos"/>
             <Route path="/photos" component={Photo} />
+            <Route path="/sign-in" component= {SignIn}/>
             <Route component={NotFound}/>
           </Switch>
         </BrowserRouter>
---
+++ src/features/Auth/pages/SignIn/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function SignIn(props) {
+    return (
+        <div className="text-center">
+            <h2>Form Login</h2>
+            <p>or login with social account</p>
+        </div>
+    )
+}
+
+SignIn.propTypes = {
+
+}
+
+export default SignIn
+
